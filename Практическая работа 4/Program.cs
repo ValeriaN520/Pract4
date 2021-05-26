635b364644f86331e9747bfde2f8f06e8f3d759e
ValeriaN520 <84538545+ValeriaN520@users.noreply.github.com>
2021-05-26 20:54:10 +0500

    Add files via upload

---
+++ Практическая работа 4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+namespace Практическая_работа_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList l1 = new ArrayList() { 'j', 6, 'i', 'S', 'O', 8, 'Y', 12, 13, 't', 'z', 'h', 5, 'L' };
+            string t = "";
+            for (int i = 0; i < l1.Count; i++)
+            {
+                t += l1[i] + "  ";
+            }
+            Console.WriteLine("Начальная коллекция: " + t);
+            ArrayList nl = new ArrayList();
+            string a = "";
+            for (int i = 0; i < l1.Count; i++)
+            {
+                if (Char.IsNumber(l1[i].ToString().ToCharArray()[0]) == false)
+                {
+                    if (Char.IsUpper(l1[i].ToString().ToCharArray()[0]))
+                    {
+                        nl.Add(l1[i]);
+                    }
+                    l1.RemoveAt(i);
+                    i--;
+                }
+                else
+                {
+                    a += l1[i] + "  ";
+                }
+            }
+            Console.WriteLine("Коллекция без букв: " + a);
+            a = "";
+            for (int i = 0; i < nl.Count; i++)
+            {
+                a += nl[i] + "  ";
+            }
+            Console.WriteLine("Коллекция заглавных букв: " + a);
+        }
+    }
+}
